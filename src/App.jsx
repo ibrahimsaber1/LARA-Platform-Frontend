2eeb3b3e593773bbf878f27e75cd9c42c220d496
IBRAHIM SABER <ibrahimsaber622@gmail.com>
2024-11-17 16:20:47 +0200

    remove the header and footer in the login and sign up

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,42 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+  import React from 'react';
+  import { BrowserRouter as Router, Routes, Route,useLocation } from 'react-router-dom';
 
-import Login from './Components/Pages/Login/Login';
-import Signup from './Components/Pages/Signup/Signup';
-import Header from './Components/Header/Header';
-import Footer from './Components/Footer/Footer';
-import Home from './Components/Pages/Home/Home';
+  import Login from './Components/Pages/Login/Login';
+  import Signup from './Components/Pages/Signup/Signup';
+  import Header from './Components/Header/Header';
+  import Footer from './Components/Footer/Footer';
+  import Home from './Components/Pages/Home/Home';
 
-function App() {
-  return (
-    <>
-    < Header/>
-    <Router>
-      
-      <Routes>
-        <Route path="" element={<Home/>} />
+  function App() {
 
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
-      </Routes>
-    </Router>
-    <Footer/>
-    </>
-  );
-}
+    const location = useLocation(); // Access the current route
+    
+    // Routes where Header and Footer should NOT appear
+      const noHeaderOrFooter = ['/login', '/signup'];
+    return (
 
-export default App;
+      <>
+        {/* Conditionally render Header and Footer */}
+        {!noHeaderOrFooter.includes(location.pathname) && <Header />}
+
+        
+        <Routes>
+          <Route path="" element={<Home/>} />
+
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+        </Routes>
+
+        {!noHeaderOrFooter.includes(location.pathname) && <Footer />}
+
+      </>
+    );
+  }
+
+  export default function AppWrapper() {
+    return (
+      <Router>
+        <App />
+      </Router>
+    );
+  }
