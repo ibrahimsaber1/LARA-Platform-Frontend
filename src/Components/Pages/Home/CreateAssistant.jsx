3390d55df18a80b1d7501a4fdf0637174fdc21b9
IBRAHIM SABER <ibrahimsaber622@gmail.com>
2024-11-25 17:06:32 +0200

    edit the create assistant styling

--- src/Components/Pages/Home/CreateAssistant.jsx
+++ src/Components/Pages/Home/CreateAssistant.jsx
@@ -30,7 +30,7 @@ const CreateAssistant = ({ onClose }) => {
           <div className="row mb-3">
             <div className="col-md-6 col-12 form-group">
               <label className="form-label">Assistant Type</label>
-              <select className="form-select">
+              <select className="form-select form-control">
                 <option>Type / Drop Down</option>
                 <option>Option 1</option>
                 <option>Option 2</option>
@@ -46,7 +46,7 @@ const CreateAssistant = ({ onClose }) => {
           <div className="row mb-3">
             <div className="col-md-6 col-12 form-group">
               <label className="form-label">Assistant Voice</label>
-              <select className="form-select">
+              <select className="form-select form-control">
                 <option>Voice Drop Down</option>
                 <option>Voice Drop Down2</option>
                 <option>Voice Drop Down3</option>
@@ -54,7 +54,7 @@ const CreateAssistant = ({ onClose }) => {
             </div>
             <div className="col-md-6 col-12 form-group">
               <label className="form-label">Assistant Domain</label>
-              <select className="form-select">
+              <select className="form-select form-control">
                 <option>Business Administration</option>
                 <option>Business Administration2</option>
                 <option>Business Administration3</option>
@@ -78,35 +78,8 @@ const CreateAssistant = ({ onClose }) => {
             <div className="col-12 form-group upload-section">
               <label>Upload File</label>
               <div className="upload-area">
-                <svg
-                  width="33"
-                  height="32"
-                  viewBox="0 0 33 32"
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    d="M21.8337 21.3333L16.5003 16L11.167 21.3333"
-                    stroke="black"
-                    strokeWidth="1.5"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                  />
-                  <path
-                    d="M16.5 16V28"
-                    stroke="black"
-                    strokeWidth="1.5"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                  />
-                  <path
-                    d="M27.6866 24.5199C28.9871 23.8109 30.0144 22.6891 30.6064 21.3314C31.1985 19.9737 31.3216 18.4575 30.9562 17.0221C30.5909 15.5868 29.7579 14.3139 28.5888 13.4045C27.4198 12.4951 25.9811 12.0009 24.4999 11.9999H22.8199C22.4164 10.4389 21.6642 8.98968 20.6199 7.76122C19.5756 6.53277 18.2664 5.55703 16.7907 4.90737C15.3151 4.25772 13.7113 3.95104 12.1001 4.01041C10.4889 4.06977 8.91204 4.49363 7.48818 5.25012C6.06433 6.0066 4.8305 7.07603 3.87944 8.37801C2.92839 9.67998 2.28487 11.1806 1.99725 12.7671C1.70964 14.3536 1.78542 15.9846 2.2189 17.5376C2.65238 19.0906 3.43227 20.525 4.49995 21.7332"
-                    stroke="black"
-                    strokeWidth="1.5"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                  />
-                </svg>{" "}
+              <i className="bi bi-file-earmark-arrow-up"></i>
+                
                 <br />
                 Drag & Drop or Click to Upload <br />
                 <span className="file-size-note">
@@ -136,7 +109,7 @@ const CreateAssistant = ({ onClose }) => {
           </div>
 
           {/* Save & Create Button */}
-          <button type="submit" className="btn btn-dark custom-btn mb-5">
+          <button type="submit" className="btn  custom-btn mb-5">
             Save & Create
           </button>
         </div>
