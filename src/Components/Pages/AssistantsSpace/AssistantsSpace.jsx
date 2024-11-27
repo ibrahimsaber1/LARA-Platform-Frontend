075a60b98f999ea59111c0dc8695da221de1872c
IBRAHIM SABER <ibrahimsaber622@gmail.com>
2024-11-27 16:38:49 +0200

    activate the arabic language in the  assistant space compounent

--- src/Components/Pages/AssistantsSpace/AssistantsSpace.jsx
+++ src/Components/Pages/AssistantsSpace/AssistantsSpace.jsx
@@ -3,20 +3,22 @@ import React, { useState } from 'react';
 import "../../../assets/styles/AssistantsSpace/AssistantsSpace.css"; 
 import { Link } from "react-router-dom";
 import StatisticsHistoryPortal from './StatisticsHistory.jsx'; // Adjust the path accordingly
+import { useTranslation } from 'react-i18next';
 
 export default function AssistantsSpace({ onOpenHistory }) {
+  const {t} = useTranslation();
   const [showStatistics, setShowStatistics] = useState(false);
   return (
     <div className="assistants-space-container">
       {/* Header Section */}
       <div className="assistants-header-section d-flex justify-content-between align-items-center">
-        <h1 className="assistants-title">Lara Assistant Title</h1>
+        <h1 className="assistants-title">{t("assistant_space.lara_assistant_title")}</h1>
         <div className="assistants-buttons">
-          <Link to={'/'} className="assistants-btn go-back-btn">Back to home</Link>
+          <Link to={'/'} className="assistants-btn go-back-btn">{t("assistant_space.back_to_home")}</Link>
 
           <div className="content">
             {/* Other content */}
-            <button className="btn Show-Statistics-btn " onClick={() => setShowStatistics(true)}>Show Statistics</button>
+            <button className="btn Show-Statistics-btn " onClick={() => setShowStatistics(true)}>{t("assistant_space.show_statistics")}</button>
             {showStatistics && <StatisticsHistoryPortal onClose={() => setShowStatistics(false)} />}
           </div>
         </div>
